00b05a3c3f30d385c9bbd10113366d27788c0ad6
paundl <paul.wandl@ecosio.com>
2015-10-08 14:30:27 +0200

    CHANGED Test from Comfort to Extendet

--- ebinterface-mappings-zugferd/src/test/java/at/austriapro/test/zugferd/ZUGFeRDMappingTest.java
+++ ebinterface-mappings-zugferd/src/test/java/at/austriapro/test/zugferd/ZUGFeRDMappingTest.java
@@ -29,7 +29,7 @@ public class ZUGFeRDMappingTest {
               "UTF-8");
 
       MappingFactory mf = new MappingFactory();
-      Mapping zugFeRDMapping = mf.getMapper(MappingFactory.MappingType.ZUGFeRD_COMFORT_1p0);
+      Mapping zugFeRDMapping = mf.getMapper(MappingFactory.MappingType.ZUGFeRD_EXTENDED_1p0);
 
       //Map to ZUGFeRD Basic
       String zugferd = new String(zugFeRDMapping.mapFromebInterface(ebInterfaceXML));
@@ -42,8 +42,6 @@ public class ZUGFeRDMappingTest {
 
       assert(false);
     }
-
-
   }
 
 
