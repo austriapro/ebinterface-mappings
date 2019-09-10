83a2076c519fdf741e346d2b40d73bd929d4fa2e
Philip Helger <philip@helger.com>
2019-09-10 22:38:55 +0200

    Using ph-ebinterface for v4.1

--- ebinterface-mappings-zugferd/src/main/java/at/austriapro/Mapping.java
+++ ebinterface-mappings-zugferd/src/main/java/at/austriapro/Mapping.java
@@ -1,11 +1,19 @@
 package at.austriapro;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+
+import javax.annotation.Nullable;
+import javax.xml.datatype.XMLGregorianCalendar;
+
 /**
  * Common interface, shared among all mappings implemented by AUSTRIAPro
  */
 public abstract class Mapping {
 
-  public MappingLog mLog = new MappingLog(MappingLogLevel.WARNING);
+  public final MappingLog mLog = new MappingLog(MappingLogLevel.WARNING);
 
   /**
    * Performs a mapping from ebInterface to a given target format, specified by the implementing
@@ -19,5 +27,51 @@ public abstract class Mapping {
 
   public String getMappingLogHTML() {
     return mLog.toHTML();
+  }
+
+
+  /**
+   * Convert the passed {@link XMLGregorianCalendar} to a
+   * {@link GregorianCalendar}.
+   *
+   * @param aCal
+   *        Source calendar. May be <code>null</code>.
+   * @return <code>null</code> if the parameter is <code>null</code>.
+   */
+  @Nullable
+  public static GregorianCalendar getGregorianCalendar (@Nullable final XMLGregorianCalendar aCal) {
+    if (aCal == null)
+      return null;
+    return aCal.toGregorianCalendar (aCal.getTimeZone (aCal.getTimezone ()),
+                                     Locale.getDefault (Locale.Category.FORMAT),
+                                     null);
+  }
+
+  /**
+   * Get the passed {@link XMLGregorianCalendar} as {@link LocalDate}.
+   *
+   * @param aCal
+   *        The source {@link XMLGregorianCalendar}. May be <code>null</code>.
+   * @return <code>null</code> if the parameter is <code>null</code>.
+   */
+  @Nullable
+  public static LocalDate getLocalDate (@Nullable final XMLGregorianCalendar aCal) {
+    if (aCal == null)
+      return null;
+    return getGregorianCalendar (aCal).toZonedDateTime ().toLocalDate ();
+  }
+
+  /**
+   * Get the passed {@link XMLGregorianCalendar} as {@link LocalDateTime}.
+   *
+   * @param aCal
+   *        The source {@link XMLGregorianCalendar}. May be <code>null</code>.
+   * @return <code>null</code> if the parameter is <code>null</code>.
+   */
+  @Nullable
+  public static LocalDateTime getLocalDateTime (@Nullable final XMLGregorianCalendar aCal) {
+    if (aCal == null)
+      return null;
+    return getGregorianCalendar (aCal).toZonedDateTime ().toLocalDateTime ();
   }
 }
--- ebinterface-mappings-zugferd/src/main/java/at/austriapro/utils/DocumentTypeUtils.java
+++ ebinterface-mappings-zugferd/src/main/java/at/austriapro/utils/DocumentTypeUtils.java
@@ -3,6 +3,9 @@ package at.austriapro.utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.helger.ebinterface.builder.EbInterfaceReader;
+import com.helger.jaxb.validation.CollectingValidationEventHandler;
+
 import java.io.StringReader;
 import java.io.StringWriter;
 
@@ -32,19 +35,12 @@ public class DocumentTypeUtils {
   static JAXBContext ebInterfaceContext4p2;
   static Unmarshaller ebInterfaceUnmarshaller4p2;
 
-  static JAXBContext ebInterfaceContext4p1;
-  static Unmarshaller ebInterfaceUnmarshaller4p1;
-
   static JAXBContext zugferdContext;
   static Unmarshaller zugferdUnmarshaller;
   static Marshaller zugferdMarshaller;
 
   static {
     try {
-      ebInterfaceContext4p1 =
-          JAXBContext.newInstance(at.austriapro.mappings.ebinterface4p1.generated.Invoice.class);
-      ebInterfaceUnmarshaller4p1 = ebInterfaceContext4p1.createUnmarshaller();
-
       ebInterfaceContext4p2 =
           JAXBContext.newInstance(at.austriapro.mappings.ebinterface4p2.generated.Invoice.class);
       ebInterfaceUnmarshaller4p2 = ebInterfaceContext4p2.createUnmarshaller();
@@ -64,43 +60,15 @@ public class DocumentTypeUtils {
   }
 
   public static MappingFactory.EbInterfaceMappingType getEbInterfaceType (String input) throws MappingException{
-    MappingFactory.EbInterfaceMappingType ebType;
-
-    try{
-      at.austriapro.mappings.ebinterface4p1.generated.Invoice inv = parseebInterface4p1(input);
+    CollectingValidationEventHandler aVEH = new CollectingValidationEventHandler ();
+    if (EbInterfaceReader.ebInterface41 ().setValidationEventHandler (aVEH).read (input) != null) 
       return MappingFactory.EbInterfaceMappingType.EBINTERFACE_4p1;
-    }catch(UnmarshalException ue){
-      try{
-        at.austriapro.mappings.ebinterface4p2.generated.Invoice inv = parseebInterface4p2(input);
-        return MappingFactory.EbInterfaceMappingType.EBINTERFACE_4p2;
-      }catch(UnmarshalException uex){
+    if (EbInterfaceReader.ebInterface42 ().setValidationEventHandler (aVEH).read (input) != null) 
+      return MappingFactory.EbInterfaceMappingType.EBINTERFACE_4p2;
+    if (EbInterfaceReader.ebInterface43 ().setValidationEventHandler (aVEH).read (input) != null) 
+      return MappingFactory.EbInterfaceMappingType.EBINTERFACE_4p3;
 
-        try{
-          at.austriapro.mappings.ebinterface4p3.generated.Invoice inv = parseebInterface4p3(input);
-          return MappingFactory.EbInterfaceMappingType.EBINTERFACE_4p3;
-        }catch(UnmarshalException uex1){
-          throw new MappingException("Unable to retrieve ebInterface version for ebInterface XML string.");
-        }
-      }
-    }
-  }
-
-  /**
-   * Parses the given ebInterface input XML string and creates a JAX-B object (ebInterface4p1)
-   */
-  public static at.austriapro.mappings.ebinterface4p1.generated.Invoice parseebInterface4p1(
-      String input) throws UnmarshalException {
-
-    StringReader reader = new StringReader(input);
-    at.austriapro.mappings.ebinterface4p1.generated.Invoice ebInterface4p1 = null;
-    try {
-      ebInterface4p1 =
-          (at.austriapro.mappings.ebinterface4p1.generated.Invoice) ebInterfaceUnmarshaller4p1
-              .unmarshal(reader);
-    } catch (JAXBException e) {
-      throw new UnmarshalException("Unable to retrieve JAX-B object (ebInterface4p1) for ebInterface XML string.");
-    }
-    return ebInterface4p1;
+    throw new MappingException("Unable to retrieve ebInterface version for ebInterface XML string.");
   }
 
   /**
