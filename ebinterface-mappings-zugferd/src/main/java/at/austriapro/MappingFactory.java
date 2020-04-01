1187c4b2924bca09ae3e6d6b62427d48e5b42feb
Philip Helger <philip@helger.com>
2020-04-01 22:51:43 +0200

    Version bump

--- ebinterface-mappings-zugferd/src/main/java/at/austriapro/MappingFactory.java
+++ ebinterface-mappings-zugferd/src/main/java/at/austriapro/MappingFactory.java
@@ -11,41 +11,47 @@ import at.austriapro.mappings.zugferd.ZUGFeRDMappingFromEbInterface4p3;
 /**
  * Creates a Mapping of a given type
  */
-public class MappingFactory {
+public class MappingFactory
+{
 
-  //The currently supported mapping types
-  public enum ZugferdMappingType {
-    ZUGFeRD_BASIC_1p0, ZUGFeRD_COMFORT_1p0, ZUGFeRD_EXTENDED_1p0
+  // The currently supported mapping types
+  public enum ZugferdMappingType
+  {
+    ZUGFeRD_BASIC_1p0,
+    ZUGFeRD_COMFORT_1p0,
+    ZUGFeRD_EXTENDED_1p0
   }
 
-  //Aggregation of all ZUGFeRD sub types
-  private static final EnumSet<ZugferdMappingType>
-      ZUGFeRDTYPES =
-      EnumSet.of(ZugferdMappingType.ZUGFeRD_BASIC_1p0, ZugferdMappingType.ZUGFeRD_COMFORT_1p0,
-                 ZugferdMappingType.ZUGFeRD_EXTENDED_1p0);
+  // Aggregation of all ZUGFeRD sub types
+  private static final EnumSet <ZugferdMappingType> ZUGFeRDTYPES = EnumSet.allOf (ZugferdMappingType.class);
 
   /**
    * Create a mapper of the given type
    */
-  public Mapping getMapper(ZugferdMappingType zugferdMappingType, EEbInterfaceVersion ebInterfaceMappingType) {
+  public Mapping getMapper (final ZugferdMappingType zugferdMappingType,
+                            final EEbInterfaceVersion ebInterfaceMappingType)
+  {
 
-    if (ZUGFeRDTYPES.contains(zugferdMappingType)) {
-      return createZUGFeRDMapper(zugferdMappingType, ebInterfaceMappingType);
-    } else {
-      throw new UnsupportedOperationException(
-          "Unable to create mapper. Only ZUGFeRD supported at the moment.");
-    }
+    if (ZUGFeRDTYPES.contains (zugferdMappingType))
+      return createZUGFeRDMapper (zugferdMappingType, ebInterfaceMappingType);
 
+    throw new UnsupportedOperationException ("Unable to create mapper. Only ZUGFeRD supported at the moment.");
   }
 
   /**
    * Create a ZUGFeRD mapper
    */
-  private Mapping createZUGFeRDMapper(ZugferdMappingType zugferdMappingType, EEbInterfaceVersion ebInterfaceMappingType) {
-    switch (ebInterfaceMappingType) {
-      case V41: return new ZUGFeRDMappingFromEbInterface4p1(zugferdMappingType);
-      case V42: return new ZUGFeRDMappingFromEbInterface4p2(zugferdMappingType);
-      case V43: return new ZUGFeRDMappingFromEbInterface4p3(zugferdMappingType);
+  private Mapping createZUGFeRDMapper (final ZugferdMappingType zugferdMappingType,
+                                       final EEbInterfaceVersion ebInterfaceMappingType)
+  {
+    switch (ebInterfaceMappingType)
+    {
+      case V41:
+        return new ZUGFeRDMappingFromEbInterface4p1 (zugferdMappingType);
+      case V42:
+        return new ZUGFeRDMappingFromEbInterface4p2 (zugferdMappingType);
+      case V43:
+        return new ZUGFeRDMappingFromEbInterface4p3 (zugferdMappingType);
     }
     return null;
   }
